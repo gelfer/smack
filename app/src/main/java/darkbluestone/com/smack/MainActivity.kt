530a52d2df2f39aae684a4b111228f6b7be14902
Gelfer <gelferstone@gmail.com>
2019-06-04 13:55:39 +0700

    lesson 3

--- app/src/main/java/darkbluestone/com/smack/MainActivity.kt
+++ app/src/main/java/darkbluestone/com/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package darkbluestone.com.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -43,7 +44,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked (view: View) {
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClicked (view: View) {
