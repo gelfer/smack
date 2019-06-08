ac24ad062bffda4efe9e2e7c7403c1a72da48e2d
Gelfer <gelferstone@gmail.com>
2019-06-08 23:54:32 +0700

    lesson 12

--- app/src/main/java/darkbluestone/com/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/darkbluestone/com/smack/Controller/CreateUserActivity.kt
@@ -1,12 +1,16 @@
 package darkbluestone.com.smack.Controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.LocalBroadcastManager
 import android.view.View
+import android.widget.Toast
 import darkbluestone.com.smack.R
 import darkbluestone.com.smack.Services.AuthService
 import darkbluestone.com.smack.Services.UserDataService
+import darkbluestone.com.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -18,9 +22,10 @@ class CreateUserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
+        createSpinner.visibility = View.INVISIBLE
     }
 
-    fun generateUserAvatar (view: View) {
+    fun generateUserAvatar(view: View) {
         val random = Random()
         val color = random.nextInt(2)
         val avatar = random.nextInt(28)
@@ -35,13 +40,13 @@ class CreateUserActivity : AppCompatActivity() {
         createAvatarImageView.setImageResource(resourceId)
     }
 
-    fun generateColorClicked (view: View) {
+    fun generateColorClicked(view: View) {
         val random = Random()
         val r = random.nextInt(255)
         val g = random.nextInt(255)
         val b = random.nextInt(255)
 
-        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+        createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
 
         val savedR = r.toDouble() / 255
         val savedG = g.toDouble() / 255
@@ -51,31 +56,70 @@ class CreateUserActivity : AppCompatActivity() {
 
     }
 
-    fun createUserClicked (view: View) {
+    fun createUserClicked(view: View) {
+
+        enableSpinner(true)
 
         val userName = createUserNameTxt.text.toString()
         val email = createEmailTxt.text.toString()
         val password = createPasswordTxt.text.toString()
 
-        AuthService.registerUser(this,email,password) { registerSuccess ->
-            if (registerSuccess) {
+        if (userName.isNotEmpty() && email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.registerUser(this, email, password) { registerSuccess ->
+                if (registerSuccess) {
                     AuthService.loginUser(this, email, password) { loginSuccess ->
                         if (loginSuccess) {
-                            AuthService.createUser(this, userName, email, userAvatar, avatarColor) {createSuccess ->
+                            AuthService.createUser(this, userName, email, userAvatar, avatarColor) { createSuccess ->
                                 if (createSuccess) {
-                                    println(UserDataService.avatarName)
-                                    println(UserDataService.avatarColor)
-                                    println(UserDataService.name)
+
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+
+                                    enableSpinner(false)
                                     finish()
+                                } else {
+                                    errorToast()
                                 }
 
                             }
 
+                        } else {
+                            errorToast()
                         }
+                    }
+                } else {
+                    errorToast()
                 }
-            }
 
+            }
+        } else {
+            Toast.makeText(
+                this, "Make sure user name, email, and password are filled in.",
+                Toast.LENGTH_LONG
+            ).show()
+            enableSpinner(false)
         }
+
+    }
+
+    fun errorToast() {
+        Toast.makeText(
+            this, "Something went wrong. Please try again.",
+            Toast.LENGTH_LONG
+        ).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        if (enable) {
+            createSpinner.visibility = View.VISIBLE
+        } else {
+            createSpinner.visibility = View.INVISIBLE
+        }
+
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
     }
 
 }
